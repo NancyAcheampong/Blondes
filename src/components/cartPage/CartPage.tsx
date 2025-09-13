8ccc7561f12bac10c36ee1d9945dcd2911940bdb
NancyAcheampong <nancyhanns1@gmail.com>
2025-09-13 21:53:34 +0000

    Link to cart

---
+++ src/components/cartPage/CartPage.tsx
@@ -0,0 +1,11 @@
+import styles from './CartPage.module.css'
+
+const CartPage = () => {
+    return ( 
+        <div className={styles.cartPage}>
+            Check What's in my cart
+        </div>
+     );
+}
+ 
+export default CartPage;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,23 +1,25 @@
-// import { Routes, Route, Link } from 'react-router-dom';
-import { ShoppingCart } from 'lucide-react';
-import styles from './Header.module.css';
+import { Routes, Route, Link } from "react-router-dom";
+import { ShoppingCart } from "lucide-react";
+import styles from "./Header.module.css";
+import CartPage from "../cartPage/CartPage";
 
 const Header = () => {
-    return (
-        <div>
-        <header className={styles.header}>
-            <div className={styles.logo}>
-                Blondes
-              </div>
-            <nav>
-               
-            </nav>
-            <div className={styles.cartIcon}>
-                <ShoppingCart size={44} />
-            </div>
-        </header>
-        </div> 
-     );
-}
- 
-export default Header; 
+  return (
+    <div>
+      <header className={styles.header}>
+        <div className={styles.logo}>Blondes</div>
+        <nav></nav>
+
+        <Link to="/cart" className={styles.cartLink}>
+          <ShoppingCart size={44} />
+        </Link>
+      </header>
+        <Routes>
+            <Route path="/cart" element={<CartPage />} />
+        </Routes>
+    </div>
+
+  );
+};
+
+export default Header;
